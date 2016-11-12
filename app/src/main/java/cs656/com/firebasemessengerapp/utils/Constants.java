ae8cbbf2e7d8d53da324deba72b1b36b4b937bc5
estwanick <estwanick@gmail.com>
2016-11-12 17:20:40 -0500

    Adds Firebase Reference

---
+++ app/src/main/java/cs656/com/firebasemessengerapp/utils/Constants.java
@@ -0,0 +1,9 @@
+package cs656.com.firebasemessengerapp.utils;
+
+public final class Constants {
+
+    //Location of Firebase
+    public static final String FIREBASE_URL = "https://fir-messenger-f2290.firebaseio.com";
+
+
+}
