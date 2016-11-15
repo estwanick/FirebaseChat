bc5055264c97118f71320e5c6d7606feb7701c61
estwanick <estwanick@gmail.com>
2016-11-14 20:21:43 -0500

    Adds initial User object

--- app/src/main/java/cs656/com/firebasemessengerapp/model/User.java
+++ app/src/main/java/cs656/com/firebasemessengerapp/model/User.java
@@ -6,7 +6,27 @@ public class User {
 
     private String username;
     private String email;
+    private HashMap<String, Object> timestampJoined;
 
+    public User(){
 
+    }
 
+    public User(String name, String email, HashMap<String, Object> timestampJoined){
+        this.username = name;
+        this.email = email;
+        this.timestampJoined = timestampJoined;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public HashMap<String, Object> getTimestampJoined() {
+        return timestampJoined;
+    }
 }
