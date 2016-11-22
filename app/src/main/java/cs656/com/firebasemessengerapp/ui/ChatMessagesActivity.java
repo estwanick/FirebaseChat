c99cefce9b53b05787786495b8045480d1041764
estwanick <estwanick@gmail.com>
2016-11-21 20:55:43 -0500

    Adds completion handler to send, erases sent message

--- app/src/main/java/cs656/com/firebasemessengerapp/ui/ChatMessagesActivity.java
+++ app/src/main/java/cs656/com/firebasemessengerapp/ui/ChatMessagesActivity.java
@@ -2,10 +2,12 @@ package cs656.com.firebasemessengerapp.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -13,6 +15,8 @@ import android.widget.TextView;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -91,7 +95,13 @@ public class ChatMessagesActivity extends AppCompatActivity {
         HashMap<String,Object> messageObj = (HashMap<String, Object>) new ObjectMapper()
                 .convertValue(message, Map.class);
         messageItemMap.put("/" + pushKey, messageObj);
-        mMessageDatabaseReference.updateChildren(messageItemMap);
+        mMessageDatabaseReference.updateChildren(messageItemMap)
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        mMessageField.setText("");
+                    }
+                });
     }
 
     private void showMessages() {
