c92372e305ed89ddc1b15b11560c07273b4b0314
estwanick <estwanick@gmail.com>
2016-11-21 20:39:22 -0500

    Refine sending message functionaliy

--- app/src/main/java/cs656/com/firebasemessengerapp/ui/ChatMessagesActivity.java
+++ app/src/main/java/cs656/com/firebasemessengerapp/ui/ChatMessagesActivity.java
@@ -4,25 +4,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import cs656.com.firebasemessengerapp.R;
-import cs656.com.firebasemessengerapp.model.Friend;
 import cs656.com.firebasemessengerapp.model.Message;
 import cs656.com.firebasemessengerapp.utils.Constants;
 
 public class ChatMessagesActivity extends AppCompatActivity {
 
     private String messageId;
+    private TextView mMessageField;
+    private Button mSendButton;
     private String chatName;
     private ListView mMessageList;
     private Toolbar mToolBar;
@@ -31,6 +37,7 @@ public class ChatMessagesActivity extends AppCompatActivity {
     private DatabaseReference mMessageDatabaseReference;
     private FirebaseListAdapter<Message> mMessageListAdapter;
     private FirebaseAuth mFirebaseAuth;
+    private Message mMessage;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -48,16 +55,43 @@ public class ChatMessagesActivity extends AppCompatActivity {
         initializeScreen();
         mToolBar.setTitle(chatName);
         showMessages();
+        addListeners();
     }
 
-    private void initializeScreen() {
-        mMessageList = (ListView) findViewById(R.id.messageListView);
-        mToolBar = (Toolbar) findViewById(R.id.toolbar);
+    public void addListeners(){
+        mMessageField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
 
-        mFirebaseDatabase = FirebaseDatabase.getInstance();
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        mMessageDatabaseReference = mFirebaseDatabase.getReference().child(Constants.MESSAGE_LOCATION
-                + "/" + messageId);
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.toString().trim().length() > 0) {
+                    mSendButton.setEnabled(true);
+                } else {
+                    mSendButton.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+    }
+
+    public void sendMessage(View view){
+        //final DatabaseReference messageRef = mFirebaseDatabase.getReference(Constants.MESSAGE_LOCATION);
+        final DatabaseReference pushRef = mMessageDatabaseReference.push();
+        final String pushKey = pushRef.getKey();
+
+        String messageString = mMessageField.getText().toString();
+        Message message = new Message(mFirebaseAuth.getCurrentUser().getEmail(), messageString);
+        //Create HashMap for Pushing
+        HashMap<String, Object> messageItemMap = new HashMap<String, Object>();
+        HashMap<String,Object> messageObj = (HashMap<String, Object>) new ObjectMapper()
+                .convertValue(message, Map.class);
+        messageItemMap.put("/" + pushKey, messageObj);
+        mMessageDatabaseReference.updateChildren(messageItemMap);
     }
 
     private void showMessages() {
@@ -69,6 +103,18 @@ public class ChatMessagesActivity extends AppCompatActivity {
             }
         };
         mMessageList.setAdapter(mMessageListAdapter);
+    }
+
+    private void initializeScreen() {
+        mMessageList = (ListView) findViewById(R.id.messageListView);
+        mToolBar = (Toolbar) findViewById(R.id.toolbar);
+        mMessageField = (TextView)findViewById(R.id.messageToSend);
+        mSendButton = (Button)findViewById(R.id.sendButton);
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mMessageDatabaseReference = mFirebaseDatabase.getReference().child(Constants.MESSAGE_LOCATION
+                + "/" + messageId);
     }
 
 }
